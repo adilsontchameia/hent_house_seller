7708a618bd05ce9044b210a4985fb3a8bfefdc72
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-05-13 21:39:37 +0100

    feat: details imports

---
+++ lib/features/presentation/sale_detail/sale_details.dart
@@ -0,0 +1,8 @@
+export 'package:animate_do/animate_do.dart';
+export 'package:cached_network_image/cached_network_image.dart';
+export 'package:flutter/material.dart';
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+export 'package:readmore/readmore.dart';
+
+export 'package:share_plus/share_plus.dart';
+export 'package:url_launcher/url_launcher.dart';
