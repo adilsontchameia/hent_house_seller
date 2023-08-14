0bba306d75edee94187813f64ed5c9f197c9797a
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-14 19:00:34 +0100

    feat: exchanging message

--- lib/features/services/chat_service.dart
+++ lib/features/services/chat_service.dart
@@ -1,10 +1,14 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hent_house_seller/features/presentation/home_screen/home.dart';
 import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
 import '../../core/factories/dialogs.dart';
 
-class ChatService {
+class ChatService extends ChangeNotifier {
   late FirebaseFirestore firestore;
   late FirebaseAuth auth;
   final ShowAndHideDialogs _dialogs = ShowAndHideDialogs();
@@ -14,13 +18,14 @@ class ChatService {
     auth = FirebaseAuth.instance;
   }
 
-  sendTextMessage({
+  void sendTextMessage({
     required String message,
     required String userId,
   }) async {
     try {
       final timeSent = DateTime.now();
       final currentId = auth.currentUser!.uid;
+      final messageId = const Uuid().v1();
       await FirebaseFirestore.instance
           .collection('messages')
           .doc(userId)
@@ -28,8 +33,10 @@ class ChatService {
           .add({
         'senderId': currentId,
         'receiverId': userId,
+        'messageId': messageId,
         'message': message,
         'date': timeSent,
+        'isSeen': false,
       });
 
       // Update last message for the seller
@@ -50,7 +57,9 @@ class ChatService {
         'senderId': currentId,
         'receiverId': userId,
         'message': message,
+        'messageId': messageId,
         'date': timeSent,
+        'isSeen': false,
       });
 
       // Update last message for the current user
@@ -60,6 +69,28 @@ class ChatService {
       });
     } catch (e) {
       _dialogs.showToastMessage(e.toString());
+    }
+  }
+
+  Future<void> setChatMessageSeen(String userId, String messageId) async {
+    try {
+      await FirebaseFirestore.instance.runTransaction((transaction) async {
+        final chatDocRef = FirebaseFirestore.instance
+            .collection('messages')
+            .doc(userId)
+            .collection('chats')
+            .doc(messageId);
+
+        final chatSnapshot = await transaction.get(chatDocRef);
+
+        if (chatSnapshot.exists) {
+          if (!chatSnapshot.data()!['isSeen']) {
+            transaction.update(chatDocRef, {'isSeen': true});
+          }
+        }
+      });
+    } catch (e) {
+      log('Error updating isSeen: $e');
     }
   }
 
