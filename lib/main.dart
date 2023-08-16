dff09d94f5eeb07abf2252caf1fa964d820c07fe
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-16 14:19:08 +0100

    feat: removed provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hent_house_seller/app/my_app.dart';
-import 'package:hent_house_seller/core/utils.dart';
 import 'package:hent_house_seller/features/presentation/providers/advertisement_provider.dart';
 import 'package:hent_house_seller/features/presentation/providers/user_data_provider.dart';
 import 'package:hent_house_seller/features/presentation/providers/user_provider.dart';
@@ -23,10 +22,6 @@ void main() async {
       providers: [
         ChangeNotifierProvider<HomeAdvertisementProvider>(
           create: (_) => HomeAdvertisementProvider(),
-          lazy: false,
-        ),
-        ChangeNotifierProvider<AppUtilsProvider>(
-          create: (_) => AppUtilsProvider(),
           lazy: false,
         ),
         ChangeNotifierProvider<UserAuthProvider>(
