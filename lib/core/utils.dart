7179b28a1a68bed323c6dac21a2acf56403ed548
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-19 19:35:55 +0100

    feat: format date

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,9 +1,7 @@
-import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 
-class AppUtilsProvider extends ChangeNotifier {
   void showSnackBar(BuildContext context, String content) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
@@ -19,27 +17,10 @@ class AppUtilsProvider extends ChangeNotifier {
     );
   }
 
-  chooseImage(
-    bool isFromGallery,
-  ) async {
-    XFile? fileImage;
-    try {
-      final picker = ImagePicker();
-      final pickedFile = await picker.pickImage(
-        source: isFromGallery ? ImageSource.gallery : ImageSource.camera,
-        imageQuality: 20,
-      );
-
-      if (pickedFile != null) {
-        fileImage = XFile(pickedFile.path);
-      } else {
-        debugPrint('No Image Selected');
-      }
-    } catch (e) {
-      log('Image: $e');
-    }
-
-    notifyListeners();
-    return fileImage!;
+  String formatDateTime(DateTime date) {
+    String formattedDate = DateFormat('HH:mm / yyyy-MM-dd').format(date);
+    return formattedDate;
   }
-}
+
+  
+
