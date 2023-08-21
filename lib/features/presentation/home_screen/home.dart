e6a76c945b6c9ffbffc817271722a9130964dc41
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-21 20:58:06 +0100

    feat: imports

---
+++ lib/features/presentation/home_screen/home.dart
@@ -0,0 +1,5 @@
+export 'package:animate_do/animate_do.dart';
+export 'package:flutter/material.dart';
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+export 'package:card_swiper/card_swiper.dart';
+export 'package:cached_network_image/cached_network_image.dart';
